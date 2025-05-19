14b8f7c45146d1670a1f76bdc2e9211cfd955781
Samingyeong <samg2003@naver.com>
2025-05-19 16:19:16 +0900

    FIX : 서버 실행 오류 수정

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Get host and port from environment variables or use defaults
 host = os.getenv("API_HOST", "127.0.0.1")
-port = int(os.getenv("API_PORT", "8000"))
+port = int(os.getenv("API_PORT", "8001"))
 
 if __name__ == "__main__":
     print(f"Starting Carbon Neutral Vision API server at http://{host}:{port}")
